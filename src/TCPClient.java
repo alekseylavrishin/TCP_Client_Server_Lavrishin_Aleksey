17eaefacd7ebfae110c44b998b2e1f480f1c572c
alekseylavrishin <78001914+alekseylavrishin@users.noreply.github.com>
2024-09-26 00:13:34 -0400

    Implemented TCPClient

---
+++ src/TCPClient.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+    public static void main(String args[]) throws IOException {
+        if(args.length != 2){
+            System.out.println("Proper input format must be 'java TCPClient <server_ip> <port>'");
+            return;
+        }
+        String serverIP = null;
+        int port = -1;
+
+        try {
+            serverIP = args[0];
+            port = Integer.parseInt(args[1]);
+        }
+        catch (Exception e){
+            System.out.println("<server_ip> must be type String and <port> must be type int");
+        }
+        Socket s = null;
+
+        try{
+            // Connect to server
+            s = new Socket(serverIP, port);
+
+            // Input and Output streams for communication with server
+            DataInputStream in = new DataInputStream( s.getInputStream());
+            DataOutputStream out = new DataOutputStream( s.getOutputStream());
+
+            // Get user input
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter text: ");
+
+            // Send user input to server
+            out.writeUTF(input.toString());
+
+            String data = in.readUTF();
+            System.out.println("Received: " + data);
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(s != null && !s.isClosed()) {
+                s.close();
+            }
+        }
+
+
+    }
+
+}
